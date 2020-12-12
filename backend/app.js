a5bbfdadefa4f0d99e56115af900369f793c85c8
Aboubakhr <Aboubakhr95@gmail.com>
2020-12-12 12:38:14 +0100

    first

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/api/posts',(req, res, next) => {
+    const posts =[
+       { 
+         id: 'fadf12421l', 
+         title: 'First server-side post', 
+         content: 'This is coming from the server '
+        },
+     
+        { 
+         id: 'ksajflaj132', 
+         title: 'First server-side post', 
+         content: 'This is coming from the server '
+        }
+    ];
+    res.status(200).json({
+    message: 'posts fetched succesfully!',
+    posts: posts
+    });
+});
+
+module.exports = app;
